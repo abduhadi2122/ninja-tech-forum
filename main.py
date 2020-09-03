023afae6d14ac1ad44e4085d7059239452a7d787
Hadi Abdul <abdulhadi2122@gmail.com>
2020-09-03 16:10:11 -0500

    added logging

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from handlers.comment import comment_handlers
 from handlers.topic import topic_handlers
 from models.settings import db
 
+import logging
+
+
+
 app = Flask(__name__)
 app.register_blueprint(auth_handlers)
 app.register_blueprint(topic_handlers)
 app.register_blueprint(comment_handlers)
+
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 # create tables in a database (important: this does not update tables, only creates new ones)
 db.create_all()
