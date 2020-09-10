437488d4d0aa3338f19ced4864ce846ee6b58a9f
Hadi Abdul <abdulhadi2122@gmail.com>
2020-09-10 18:26:36 -0500

    sending email with a worker

--- utils/email_helper.py
+++ utils/email_helper.py
@@ -1,40 +1,18 @@
-import json
+
 import os
-import requests
+from tasks import send_email_task
 
 
 def send_email(receiver_email, subject, text):
-    sender_email = os.getenv("MY_SENDER_EMAIL")  # Your website's official email address
-    api_key = os.getenv('SENDGRID_API_KEY')
+    if os.getenv('REDIS_URL'):
+        # if the web app is on Heroku, call the "send email" task
+        send_email_task(receiver_email, subject, text)
 
-    if sender_email and api_key:
-        url = "https://api.sendgrid.com/v3/mail/send"
-
-        data = {"personalizations": [{
-                    "to": [{"email": receiver_email}],
-                    "subject": subject
-                }],
-
-                "from": {"email": sender_email},
-
-                "content": [{
-                    "type": "text/plain",
-                    "value": text
-                }]
-        }
-
-        headers = {
-            'authorization': "Bearer {0}".format(api_key),
-            'content-type': "application/json"
-        }
-
-        response = requests.request("POST", url=url, data=json.dumps(data), headers=headers)
-
-        print("Sent to SendGrid")
-        print(response.text)
     else:
-        print("No env vars or no email address.")
-        print("The email was not sent.")
-        print("If it was sent, this would be the subject: {}".format(subject))
-        print("This would be the text: {}".format(text))
-        print("And this would be the receiver: {}".format(receiver_email))
+        # but if you're on localhost, just simulate sending email in the console
+        print("You are on localhost, so the email will not be really sent.")
+        print("---------------EMAIL MESSAGE----------------")
+        print("Email recipient: {}".format(receiver_email))
+        print("Subject: {}".format(subject))
+        print(text)
+        print("---------------EMAIL MESSAGE----------------")
